0bd103ed92a1ea55939ba3c730532237553f9e13
Igor Barros <barrosigor74@gmail.com>
2022-03-05 06:58:56 -0300

    Adicionando objeto na memória.

--- projeto-java-poo/src/pacote/principal/App.java
+++ projeto-java-poo/src/pacote/principal/App.java
@@ -1,9 +1,16 @@
 package pacote.principal;
 
+import pacote.classe.Aluno;
+
 public class App {
 	
 	/* Main é um método auto executável em Java */
 	public static void main(String[] args) {
 		
+		/* Objeto não está na memória */
+		Aluno aluno1;
+		
+		/* Objeto na memória */
+		Aluno aluno2 = new Aluno();
 	}
 }
