ad8cc12d3a87587ca4735fce21a76bb4f9ad86f1
Igor Barros <barrosigor74@gmail.com>
2022-03-05 06:41:32 -0300

    Criação da classe Aluno.

---
+++ projeto-java-poo/src/pacote/classe/Aluno.java
@@ -0,0 +1,16 @@
+package pacote.classe;
+
+/* Classe/Objeto que representa o Aluno */
+public class Aluno {
+
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+}
--- projeto-java-poo/src/pacote/principal/App.java
+++ projeto-java-poo/src/pacote/principal/App.java
@@ -2,6 +2,8 @@ package pacote.principal;
 
 public class App {
 	
-	/**/
-
+	/* Main é um método auto executável em Java */
+	public static void main(String[] args) {
+		
+	}
 }
