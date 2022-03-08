658d4782179b21329cd60d8333ab115187221867
Igor Barros <barrosigor74@gmail.com>
2022-03-08 02:54:12 -0300

    Os metódos comuns Getters e Setters.

--- projeto-java-poo/src/pacote/classe/Aluno.java
+++ projeto-java-poo/src/pacote/classe/Aluno.java
@@ -5,16 +5,20 @@ public class Aluno {
 
 	/* Esses são os atributos do Aluno, representa as características do mundo real */
 	// Por padrão os atributos são privados.
-	public String nome;
-	public int idade;
-	String dataNascimento;
-	String registroGeral;
+	/* Private é para manter o controle de acesso a variáveis,
+	 *  evitando que o programador chame a variável de forma errada,
+	 *  alterando o valor dela causando erro no sistema
+	 */
+	/*public*/ private String nome;
+	/*public*/ public int idade;
+	private String dataNascimento;
+	private String registroGeral;
 	private String numeroCpf;
-	String nomeMae;
-	String nomePai;
-	String dataMatricula;
-	String nomeEscola;
-	String serieMatriculado;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
 	
 	/* Método construtor - Cria os dados na memória, sendo padrão do Java */
 	public Aluno() {
@@ -29,4 +33,93 @@ public class Aluno {
 		nome = nomePadrao;
 		idade = idadePadrao;
 	}
+	
+	/*Métodos GETTERS E SETTERS do objeto*/
+	//SET é para adicionar ou receber dados dos atributos
+	//GET é para obter o valores dos atributos
+	
+	//Método SET que recebe dado
+	public void setNome(String nome /*nome = paramêtro*/) {
+		this.nome = nome; //this = operador do Java
+	}
+	
+	//Método GET que busca o dado
+	public String getNome() {
+		return nome;
+	}	
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	/* Gerando automaticamente os GETTERS E SETTERS
+	 * botão direito >> Source >> Generate Getters and Setters
+	 */
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}	
 }
--- projeto-java-poo/src/pacote/principal/App.java
+++ projeto-java-poo/src/pacote/principal/App.java
@@ -10,22 +10,47 @@ public class App {
 		/* Objeto não está na memória, ele não existe */
 		Aluno aluno1;
 		
+		/*======================================================================*/
+		
 		/* Objeto está na memória, new = cria um novo objeto */
 		/* Método construtor */
 		Aluno aluno2 = new Aluno(); /* Aqui será Maria */
 		// Declarando o nome
-		aluno2.nome = "Maria";
+		aluno2.setNome("Maria");
 		aluno2.idade = 16;
+		aluno2.setDataNascimento("30/11/2006");
+		aluno2.setRegistroGeral("85215412");
+		aluno2.setNumeroCpf("155.055.152-56");
+		aluno2.setNomeMae("Joana");
+		aluno2.setNomePai("Carlos");
+		aluno2.setDataMatricula("10/02/2002");
+		aluno2.setSerieMatriculado("9");
+		aluno2.setNomeEscola("Colégio o Santuário Java");
 		
 		// + = Concatenação
-		System.out.println("Nome da Aluna 2 é: " + aluno2.nome);
+		System.out.println("Nome da Aluna 2 é: " + aluno2.getNome());
 		System.out.println("A idade da Aluna 2 é: " + aluno2.idade);
+		System.out.println("Nascimento: " + aluno2.getDataNascimento());
+		
+		/*======================================================================*/
 		
 		/* new Aluno(); é uma instância, estamos instanciando o objeto */ 
 		/* aluno3 é um referência ao Aluno */
 		Aluno aluno3 = new Aluno(); /* Aqui será o José */
 		
+		aluno3.setNome("José");
+		aluno3.idade = 17;
+		aluno3.setDataNascimento("09/09/2005");
+		
+		System.out.println("Nome do Aluno 3 é: " + aluno3.getNome());
+		System.out.println("A idade do Aluno 3 é: " + aluno3.idade);
+		System.out.println("Nascimento: " + aluno3.getDataNascimento());
+		
+		/*======================================================================*/
+		
 		Aluno aluno4 = new Aluno("Maria"); // Passando objeto com valor padrão com um paramêtro.
+		
+		/*======================================================================*/
 		
 		Aluno aluno5 = new Aluno("José", 34); // Há várias tipos de métodos construtores.
 	}
